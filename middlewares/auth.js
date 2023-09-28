2912f9e0a48ae657dc6ea6e38d9eda3cbb10654b
Blessed Oscar <oscarblessed04@gmail.com>
2023-09-28 19:25:14 +0100

    token authentication setup

---
+++ middlewares/auth.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, '..', '.env')});
+const authSchema = require('../schema/user');
+
+
+const verifyJWTMiddleware = (req, res, next) => {
+    const authHeader = req.headers["authorization"]
+    if (!authHeader) return res.status(401).json({"message": "authorization header not set"});
+
+    const authToken = authHeader.split(' ')[1];
+    jwt.verify(
+        authToken,
+        process.env.SECRET_KEY,
+        (err, decoded) =>{
+            if (err) return res.status(403).json({"message":"authorization failed"});
+            else{
+                if(decoded.id){
+                    id = decoded.id;    
+                    user =  authSchema.userCollection.findById(id)
+                    .then(function(user) {
+                        if (!user){
+                            res.status(403).json({"message":"authorization failed"})
+                        }else{
+                            req.user = user
+                            next()
+                        }
+                        
+                    })
+
+
+                }else{
+                    res.status(403).json({"message":"authorization failed"})
+                }
+
+            }
+          
+        }
+    )
+
+
+}
+
+module.exports = verifyJWTMiddleware
--- routers/tasks.js
+++ routers/tasks.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const tasksController = require('../controllers/tasks');
+const authMiddleWare = require('../middlewares/auth');
 
-router.get('/all/', tasksController.getTasksController);
-router.post('/create/', tasksController.createTasksController);
-router.get('/single/:id/', tasksController.getSingleTaskController);
-router.put('/edit/:taskTitle/', tasksController.editSingleTaskController);
-router.delete('/delete/:id/', tasksController.deleteSingleTask);
+router.get('/all/', authMiddleWare, tasksController.getTasksController);
+router.post('/create/', authMiddleWare, tasksController.createTasksController);
+router.get('/single/:id/', authMiddleWare, tasksController.getSingleTaskController);
+router.put('/edit/:taskTitle/', authMiddleWare, tasksController.editSingleTaskController);
+router.delete('/delete/:id/',  authMiddleWare,tasksController.deleteSingleTask);
 
 module.exports = router;
 
