89f8a092ccea67c55d22170e26a5c03c566d6549
Tech Priest <oscarblessed04@gmail.com>
2023-11-21 12:28:47 +0100

    configured morgan and cors

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 const connect = mongoose.connect(process.env.DATABASE_URL);
 const authMiddleWare = require('../middlewares/auth');
-
+const cors = require("cors");
+const logger = require('morgan');
 
 connect.then(()=> {
     console.log('connected to database');
@@ -18,6 +19,16 @@ connect.then(()=> {
 app.listen(PORT, function() {
     console.log(`listening on port ${PORT}`);
 });
+
+// this specifies the allowed domains
+app.use(cors({
+    origin: process.env.ALLOWED_DOMAINS
+}));
+
+// this makes the server to be accessible to all origins
+// app.use(cors())
+
+app.use(logger(":method :url :status response-time =>  :response-time ms from [==:user-agent==]"));
 app.use(express.json());
 
 app.use('/auth', require('../routers/auth.js'));
