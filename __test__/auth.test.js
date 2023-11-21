7ba1ae5507b37c60cc66f9f97e1f748d8e1efaf4
Tech Priest <oscarblessed04@gmail.com>
2023-11-21 07:45:30 +0100

    hid secrets in test

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -1,11 +1,13 @@
 const axios =  require('axios');
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, '..', '.env')});
 
 test("Signup Test", async () => {
     const response = await axios.post("http://localhost:4000/auth/signup/", {
-        "email": "boss82413@gmail.com",
+        "email": process.env.TEST_EMAIL,
         "fullName": "Oscar Blessed",
         "role": "admin",
-        "password": "chickenkitchen",
+        "password": process.env.TEST_PASSWORD,
     });
 
     expect(response.status).toBe(201);   
@@ -14,8 +16,8 @@ test("Signup Test", async () => {
 
 test("Login Test", async () => {
     const response = await axios.post("http://localhost:4000/auth/login/", {
-        "email": "boss82413@gmail.com",
-        "password": "chickenkitchen",
+        "email": process.env.TEST_EMAIL,
+        "password": process.env.TEST_PASSWORD,
         });
     
     expect(response.status).toBe(200);   
